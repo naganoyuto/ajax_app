593ca9cdf1166cacbb21bfe02920d3d62f837956
singori <naganoyuto@icloud.com>
2021-02-12 18:01:44 +0900

    AjaxAppの準備

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,16 @@
 class PostsController < ApplicationController
-    def index
-     
+
+  def index  
+    @posts = Post.all.order(id: "DESC")
   end
+ 
+ 
+  def create
+    Post.create(content: params[:content])
+    redirect_to action: :index
+  end
+
+
+ 
+ end
   
--- db/migrate/20210124084037_create_posts.rb
+++ db/migrate/20210124084037_create_posts.rb
@@ -1,7 +1,14 @@
+
+<h1>AjaxApp</h1>
+<%= form_with url:  "/posts", method: :post,id: "form" do |form| %>
+  <%= form.text_field :content %>
+  <%= form.submit '投稿する' , id: "submit" %>
+<% end %>
 class CreatePosts < ActiveRecord::Migration[6.0]
   def change
     create_table :posts do |t|
       t.text :content
+      t.boolean :checked
       t.timestamps
     end
   end
